481c62f06f782d6f144bf61410a9f46d6ac7b967
scorpion8511 <dewan8511@gmail.com>
2025-07-30 20:20:02 +0900

    Fix module reload warning

--- pmc15_pipeline/__init__.py
+++ pmc15_pipeline/__init__.py
@@ -1,6 +1,17 @@
 """BiomedCLIP data pipeline."""
 
-from . import data, pathology_pipeline
+from importlib import import_module
+
+from . import data
 
 __all__ = ["data", "pathology_pipeline"]
+
+
+def __getattr__(name):
+    """Lazily import submodules when accessed."""
+    if name == "pathology_pipeline":
+        module = import_module(".pathology_pipeline", __name__)
+        globals()[name] = module
+        return module
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
